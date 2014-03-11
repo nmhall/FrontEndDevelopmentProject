9a10a447d6550f057658632168d760409bed1e0e
Nathan Hall <nmhall0@gmail.com>
2014-03-10 20:28:16 -0700

    Made Atwood circle locations almost right, and made the circles clicable

--- drawRoomOccupants.js
+++ drawRoomOccupants.js
@@ -8,11 +8,37 @@ var rooms = document.getElementsByTagName('area');
 
 for(var i = 0; i < rooms.length; i++) {
     rooms[i].addEventListener('click', displayRoomStatus, false);
+    var id = rooms[i].id;
+    // Concatenate '-occupied' and '-locked' to get occupancy image ids
+    var occId = id + '-occupied';
+    var lockedId = id + '-locked';
+    occDiv = document.getElementById(occId);
+    lockedDiv = document.getElementById(lockedId);
+    if (occDiv) {
+        occDiv.addEventListener('click', displayOccupiedRoomStatus, false);
+    }
+    if (lockedDiv) {
+        lockedDiv.addEventListener('click', displayOccupiedRoomStatus, false);
+    }
 }
 
 function displayRoomStatus() {
-    roomId = this.id;
+    var roomId = this.id;
     if (occupancies[roomId] == 'Locked') {
+        alert('This room is currently taken by a number with higher priority.');
+    }
+    else if (occupancies[roomId] == 'Open') {
+        alert('This room is currently taken, but by a number with lower priority. You may take it if you wish.');
+    }
+    else {
+        alert('This room is currently empty. You may take it if you wish.');
+    }
+}
+
+function displayOccupiedRoomStatus() {
+    var id = this.id;
+    var roomId = id.split('-')[0];
+    if (occupancies[roomId] ==  'Locked') {
         alert('This room is currently taken by a number with higher priority.');
     }
     else if (occupancies[roomId] == 'Open') {
