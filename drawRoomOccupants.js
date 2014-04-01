ec5160fc22c952380f8764e3bbea304cb2b52817
Nathan Hall <nmhall0@gmail.com>
2014-04-01 12:59:00 -0700

    Added putting students in rooms to every dorm floor page

--- drawRoomOccupants.js
+++ drawRoomOccupants.js
@@ -1,37 +1,100 @@
-var occupancies = {};
-// Hard-coded occupancies for Atwood, not a long-term solution
-occupancies['atwood103'] = 'Locked';
-occupancies['atwood105'] = 'Locked';
-occupancies['atwood109'] = 'Occupied';
-occupancies['atwood111'] = 'Occupied';
+var roomsDB = TAFFY()
+
+// Hard-code some placements into the database here
+
 var rooms = document.getElementsByTagName('area');
 
 for(var i = 0; i < rooms.length; i++) {
-    rooms[i].addEventListener('click', displayRoomStatus, false);
     var id = rooms[i].id;
+    rooms[i].addEventListener('click', putStudentInRoom(id), false);
     // Concatenate '-occupied' and '-locked' to get occupancy image ids
     var occId = id + '-occupied';
     var lockedId = id + '-locked';
     occDiv = document.getElementById(occId);
     lockedDiv = document.getElementById(lockedId);
     if (occDiv) {
-        occDiv.addEventListener('click', displayOccupiedRoomStatus, false);
+        occDiv.addEventListener('click', putStudentInRoom(id), false);
     }
     if (lockedDiv) {
-        lockedDiv.addEventListener('click', displayOccupiedRoomStatus, false);
+        lockedDiv.addEventListener('click', putStudentInRoom(id), false);
     }
-    if (occupancies[id] == 'Locked') {
-        lockedDiv.style.zIndex = '1';
-        occDiv.style.zIndex= '-1';
+    lockedDiv.style.zIndex = '-1';
+    occDiv.style.zIndex = '-1';
+}
+
+function putStudentInRoom(id) {
+    var toReturn = function(){
+        var fullName = document.getElementById('fullName').value;
+        if (fullName == '') {
+            alert('You must enter a name to add to the room');
+            return;
+        }
+        var prioritySelect = document.getElementById('prioritySelect');
+        var priorityLevel = prioritySelect.options[prioritySelect.selectedIndex].value;
+        alert('Priority Level is ' + priorityLevel);
+        if (priorityLevel == 'None Selected') {
+            alert('You must choose a year in order to add yourself to a room');
+            return;
+        }
+        var priorityNumberString = document.getElementById('priorityNumber').value;
+        var priorityNumber = -1;
+        if(priorityNumberString != '') {
+            priorityNumber = parseInt(priorityNumberString);
+        }
+        alert('Priority Number is ' + priorityNumber);
+        if (priorityLevel != 'Admin' && priorityNumber == -1) {
+            alert('You must enter a priority number for the room');
+            return;
+        }
+        var currentEntry = roomsDB({roomId: id});
+        if (currentEntry.count() == 0) {
+            alert('No current occupant, adding to room ' + id);
+            roomsDB.insert({roomId: id, fullName: fullName, priorityLevel: priorityLevel, priorityNumber: priorityNumber});
+            alert('You have succesfully added ' + fullName + ' to the room with priority ' + priorityLevel + ' ' + priorityNumber);
+            return;
+        }
+        else {
+            occupantInfo = currentEntry.first();
+            occPriorityLevel = occupantInfo.priorityLevel;
+            alert('Occupant Priority Level is ' + occPriorityLevel);
+            occPriorityNumber = occupantInfo.priorityNumber;
+            alert('Occupant Priority Number is ' + occPriorityNumber);
+            if (isHigherPriorityLevel(occPriorityLevel, priorityLevel)) {
+                alert('This room is currently taken with a higher priority: ' + occPriorityLevel + ' ' + occPriorityNumber);
+                return;
+            }
+            else if(occPriorityLevel == priorityLevel && occPriorityNumber < priorityNumber) {
+                alert('This room is currently taken with a higher priority: ' + occPriorityLevel + ' ' + occPriorityNumber);
+                return;
+            }
+            else {
+                roomsDB({roomId: id}).update({roomId: id, fullName: fullName, priorityLevel: priorityLevel, priorityNumber: priorityNumber});
+                alert('You have succesfully added ' + fullName + ' to the room with priority ' + priorityLevel + ' ' + priorityNumber);
+                return;
+            }
+        }
     }
-    else if (occupancies[id] == 'Occupied') {
-        lockedDiv.style.zIndex = '-1';
-        occDiv.style.zIndex = '1';
+    return toReturn;
+}
+
+// Returns true if priority is of a higher priority level than compareTo
+function isHigherPriorityLevel(priority, compareTo) {
+    if (priority == 'Admin') {
+        if (compareTo == 'Senior' || compareTo == 'Junior' || compareTo == 'Sophomore') {
+            return true;
+        }
     }
-    else {
-        lockedDiv.style.zIndex = '-1';
-        occDiv.style.zIndex = '-1';
+    else if (priority == 'Senior') {
+        if (compareTo == 'Junior' || compareTo == 'Sophomore') {
+            return true;
+        }
     }
+    else if (priority == 'Junior') {
+        if (compareTo == 'Sophomore') {
+            return true;
+        }
+    }
+    return false;
 }
 
 function displayRoomStatus() {
